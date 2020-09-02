72c9ffa1cbe6c0357a1d65cef89ee5b4a4ab006d
Yamil Lasso <ylasso@Yamils-iMac.local>
2020-09-01 23:51:34 -0500

    Creation of search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Search from './components/Search'
 
@@ -10,7 +10,13 @@ class App extends Component {
 
   render() {
     return <div>
-      <Search />
+      <Router>
+        <Route exact path="/" render={( ) => {
+          return <div>
+            <Search/>
+          </div>
+        }}></Route>
+      </Router>
     </div>
   }
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import '../scss/base.scss';
+import '../scss/components/search.scss';
+import logo from '../assets/logo_ml.png';
+import searchIcon from '../assets/ic_Search.png';
 
 export default class Search extends Component {
     render() {
         return (
             <div className="search">
-                SEARCH BAR
+                <div className="search o-container">
+                    <Link to="/">
+                        <img className="search__ml-logo" src={logo} alt="Logo" />
+                    </Link>
+                    <div class="search__container">
+                        <form>
+                            <input type="text" placeholder="Nunca dejes de buscar" name="search" />
+                            <button type="submit"><img className="search__icon" src={searchIcon} alt="Logo" /></button>
+                        </form>
+                    </div>
+                </div>
             </div>
         )
     }
